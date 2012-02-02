c965359bd8cea4d9cdbe0521662ab7a7ac450763
Max Kueng <me@maxkueng.com>
2012-02-02 22:59:14 +0100

    Exit when done.

--- redisclean.js
+++ redisclean.js
@@ -3,11 +3,27 @@ var redis = require("redis");
 var r = redis.createClient();
 r.on('connect', function () {
 	r.keys('*:*', function (err, res) {
-		var keys = res.split(" ");
+		var keys = [];
+		if (typeof res === 'object') {
+			keys = res;
+		} else {
+			keys = res.split(" ");
+		}
+		var counter = keys.length;
+
+		if (counter === 0) {
+			console.log('No keys');
+			process.exit(0);
+		}
+
+		console.log('Removing ' + counter + ' keys');
 		for (var i = 0; i < keys.length; i++) {
-			console.log(keys[i]);
 			r.del(keys[i], function (err, res) {
-				console.log(err, res);
+
+				if (!--counter) {
+					console.log('OK');
+					process.exit(0);
+				}
 			});
 		}
 	});		
