11304e28003ed6154a8c7739212bb643e5688ed9
Max Kueng <me@maxkueng.com>
2012-02-06 01:03:13 +0100

    Added ReadyList, cleaner code and more

--- test.js
+++ test.js
@@ -3,8 +3,10 @@ var path = require('path');
 var walk = require('walk');
 var mb = require('musicbrainz');
 var metaflac = require('metaflac');
-var redis = require("redis");
+var redis = require('redis');
 var program = require('commander');
+var timers = require('timers');
+var ReadyList = require('./lib/readylist').ReadyList;
 
 var app = require('http').createServer(handler);
 var io = require('socket.io').listen(app);
@@ -27,22 +29,15 @@ function handler (req, res) {
 
 io.sockets.on('connection', function (socket) {
 	socket.on('releaseactivated', function (data) {
-		console.log('activated', data);
 		io.sockets.emit('releaseactivated', data);
-		trackQueue[data.discid].release = data.release;
-		trackQueue[data.discid].activated = true;
-
-		for (var i = 0; i < trackQueue[data.discid].callbacks.length; i++) {
-			trackQueue[data.discid].callbacks[i](trackQueue[data.discid].release);
-		}
+		flacTagQueue.data(data.discid).release = data.release;
+		flacTagQueue.ready(data.discid, true);
 	});
 
 	socket.on('releaseselected', function (data) {
-		console.log('selected', data);
 		io.sockets.emit('releaseselected', data);
 	});
 });
-
 
 program
   .version('0.0.1')
@@ -52,86 +47,79 @@ program
   .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
   .parse(process.argv);
 
-
-var lookupQueue = {};
-var trackQueue = {};
-
-function queueTrack (discId, callback) {
-	if (trackQueue[discId].activated && trackQueue[discId].release) {
-		callback(trackQueue[discId].release);
-		return;
-	}
-
-	trackQueue[discId].callbacks.push(callback);
-};
+var lookupQueue = new ReadyList();
+var flacTagQueue = new ReadyList();
 
 mb.lookupCache = function (uri, callback, lookup) {
 	var key = 'lookup:' + uri;
 
-	if (typeof lookupQueue[key] === 'undefined') {
-		lookupQueue[key] = {
-			'callbacks' : [callback], 
-			'error' : null, 
-			'resource' : null, 
-			'ready' : false
-		};
+	lookupQueue.add(key, true, function (data) {
+		callback(data.error, data.resource);
+	});
 
+	if (!lookupQueue.isReady(key)) {
 		var r = redis.createClient();
 		r.on('connect', function () {
-			r.get(key, function (error, reply) {
+			r.get(key, function (err, reply) {
 				if (reply) {
-					lookupQueue[key].error = null;
-					lookupQueue[key].resource = JSON.parse(reply);
-					lookupQueue[key].ready = true;
-					for (var i = 0; i < lookupQueue[key].callbacks.length; i++) {
-						lookupQueue[key].callbacks[i](lookupQueue[key].error, lookupQueue[key].resource);
-					}
+					lookupQueue.data(key).error = null;
+					lookupQueue.data(key).resource = JSON.parse(reply);
+					lookupQueue.ready(key, true);
 					r.quit();
 
 				} else {
 					lookup(function (err, resource) {
-						lookupQueue[key].error = err;
-						lookupQueue[key].resource = resource;
-						lookupQueue[key].ready = true;
 
-						if (err) { 
-							console.log(err); 
-							for (var i = 0; i < lookupQueue[key].callbacks.length; i++) {
-								lookupQueue[key].callbacks[i](lookupQueue[key].error, lookupQueue[key].resource);
-							}
-							return; 
-						}; 
-						r.set(key, JSON.stringify(resource), function (e, re) {
-							for (var i = 0; i < lookupQueue[key].callbacks.length; i++) {
-								lookupQueue[key].callbacks[i](lookupQueue[key].error, lookupQueue[key].resource);
-							}
+						lookupQueue.data(key).error = err;
+						lookupQueue.data(key).resource = resource;
+						lookupQueue.ready(key, true);
+
+						if (err) { r.quit(); return; }
+
+						r.set(key, JSON.stringify(resource), function (err, reply) {
 							r.quit();
 						});
+
 					});
 				}
 			});
-
 		});
-	} else if (lookupQueue[key].ready === true) {
-		callback(lookupQueue[key].error, lookupQueue[key].resource);
-	} else {
-		lookupQueue[key].callbacks.push(callback);
 	}
 };
 
-function handleFLAC (resolvedPath, next) {
+function tagFLAC (discId, trackNumber, releaseId, filePath) {
+	var release = new mb.Release(releaseId);
+	release.load(['release-groups', 'recordings', 'mediums', 'labels', 'artists', 'discids'], function (error) {
+		var medium = release.getMediumByDiscId(discId);
+		if (medium) {
+			var track = medium.getTrackByPosition(trackNumber);
+			if (track) {
+				console.log(discId, medium.position, track.position, track.recording.id);
+				
+				io.sockets.emit('tracktagged', {
+					'release' : release.id, 
+					'mediumPosition' : medium.position, 
+					'trackPosition' : track.position, 
+					'recording' : track.recording.id
+				});
+			}
+		}
+	});
+};
+
+function handleFLAC (resolvedPath) {
 	metaflac.vorbisComment(resolvedPath, function (err, tags) {
-		if (err) { console.log(resolvedPath, err); next(); return; }
+		if (err) { console.log(resolvedPath, err); return; }
 
 		mb.lookupDiscId(tags['MUSICBRAINZ_DISCID'], [], function (err, disc) {
-			if (err) { console.log(resolvedPath, err.data()); next(); return; }
+			if (err) { console.log(resolvedPath, err.data()); return; }
 
-			if (typeof trackQueue[disc.id] === 'undefined') {
-				trackQueue[disc.id] = { 'releases' : {}, 'callbacks' : [], 'complete' : false, 'activated' : false, 'release' : null };
-			}
-
-			queueTrack(disc.id, function (releaseId) {
-				console.log(tags['MUSICBRAINZ_DISCID'], tags['TRACKNUMBER']);
+			flacTagQueue.add(disc.id, true, function (data) {
+				var discId = tags['MUSICBRAINZ_DISCID'];
+				var trackNumber = tags['TRACKNUMBER'];
+				var filePath = resolvedPath;
+				var releaseId = data.release;
+				tagFLAC(discId, trackNumber, releaseId, filePath);
 			});
 
 			var counter = disc.releases.length;
@@ -139,19 +127,16 @@ function handleFLAC (resolvedPath, next) {
 				(function(_i) {
 					var release = disc.releases[_i];
 					release.load(['release-groups', 'recordings', 'mediums', 'labels', 'artists'], function () {
-						if (release.isComplete()) console.log(release.id, 'COMPLETE');
-						if (typeof trackQueue[disc.id].releases[release.id] === 'undefined') {
-							trackQueue[disc.id].releases[release.id] = release;
-						}
-
-						if (typeof release.releaseGroups[0] === 'undefined') {
-							console.log('no groups:', release.id);
-						}
-
 						if (!--counter) {
-							if (!trackQueue[disc.id].complete) {
+							if (!flacTagQueue.data(disc.id, 'releasesCompleted')) {
 								io.sockets.emit('disc', disc);
-								trackQueue[disc.id].complete = true;
+								flacTagQueue.data(disc.id).releasesCompleted = true;
+
+								if (disc.releases.length == 1 && release.isComplete()) {
+									io.sockets.emit('releaseactivated', { 'discid' : disc.id, 'release' : release.id });
+									flacTagQueue.data(disc.id).release = release.id;
+									flacTagQueue.ready(disc.id, true);
+								}
 							}
 						}
 					});
@@ -171,7 +156,7 @@ function start () {
 			if (err) return;
 
 			if (path.extname(resolvedPath) === '.flac') {
-				handleFLAC(resolvedPath, next);
+				handleFLAC(resolvedPath);
 			}
 
 			next();
